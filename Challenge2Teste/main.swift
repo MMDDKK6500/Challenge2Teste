af6b7daae55fc6f9faff95ef88c4f95aa5f22083
João Duque Nardelli Wandermuren <joao.dnwandermuren@senacsp.edu.br>
2026-05-15 19:44:43 -0300

    Adicionar arquivos do projeto

---
+++ Challenge2Teste/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  Challenge2Teste
+//
+//  Created by João Duque Nardelli Wandermuren on 08/05/26.
+//
+
+import Foundation
+
+struct Produto {
+    let nome: String
+    let id: Int
+    let valor: Double
+}
+
+
+let produtos = [
+    Produto(nome: "Caneca", id: 12, valor: 19.99),
+    Produto(nome: "Copo", id: 13, valor: 9.99),
+    Produto(nome: "Lapis", id: 14, valor: 1.99),
+]
+
+func listarProdutos() {
+    for produto in produtos {
+        print("\(produto.id) - \(produto.nome): R$ \(produto.valor)")
+    }
+}
+
+listarProdutos()
